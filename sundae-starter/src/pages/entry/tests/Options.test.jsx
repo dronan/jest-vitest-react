584e9c85fdd44e89b8368a0a13a02ad39f59a818
Diego Ronan Kulian <dkulian@veepee.com>
2024-04-12 16:16:37 +0200

    findallbyrole + await to validade data async

---
+++ sundae-starter/src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from "../../../test-utils/testing-library-utils";
+import { userEvent } from "@testing-library/user-event";
+import Options from "../Options";
+
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  // find images with await to make sure they load and avoid error
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm alt text of images
+  // @ts-ignore
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
