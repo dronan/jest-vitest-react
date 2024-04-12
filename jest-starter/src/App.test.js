32555614cd0c9c8d03df75c7cef540e9d8fd7f2c
Diego Ronan Kulian <dkulian@veepee.com>
2024-04-12 13:02:34 +0200

    test click behavior

--- jest-starter/src/App.test.js
+++ jest-starter/src/App.test.js
@@ -1,35 +1,22 @@
-import { logRoles, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial color", () => {
-  const { container } = render(<App />);
-  logRoles(container);
+test("button click flow", () => {
+  // render the app
+  render(<App />);
+
+  // find the button
   const btn = screen.getByRole("button", { name: /blue/i });
+
+  // check the initial state
   expect(btn).toHaveClass("red");
+
+  // click event
+  fireEvent.click(btn);
+
+  // check the button text
+  expect(btn).toHaveTextContent(/red/i);
+
+  // check the button class
+  expect(btn).toHaveClass("blue");
 });
-
-// test("button has correct initial text", () => {
-//   render(<App />);
-//   const colorButton = screen.getByRole("button", {
-//     name: "Change to Midnight Blue",
-//   });
-//   expect(colorButton.textContent).toBe("Change to Midnight Blue");
-// });
-
-// test("button has correct color after click", () => {
-//   render(<App />);
-//   const colorButton = screen.getByRole("button", {
-//     name: "Change to Midnight Blue",
-//   });
-//   colorButton.click();
-//   expect(colorButton).toHaveStyle({ backgroundColor: "midnight-blue" });
-// });
-
-// test("button has correct text after click", () => {
-//   render(<App />);
-//   const colorButton = screen.getByRole("button", {
-//     name: "Change to Midnight Blue",
-//   });
-//   colorButton.click();
-//   expect(colorButton.textContent).toBe("Change to Medium Violet Red");
-// });
