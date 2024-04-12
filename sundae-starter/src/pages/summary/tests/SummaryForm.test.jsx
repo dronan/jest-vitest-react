303b3dad885e0d43a3c7cd7ef6e177de37af7562
Diego Ronan Kulian <dkulian@veepee.com>
2024-04-12 15:33:56 +0200

    add initial conditions

--- sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
@@ -2,6 +2,17 @@ import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
 
+test("Initial conditions", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
 test("Checkbox enables button on first click and disables on second click", async () => {
   const user = userEvent.setup();
 
