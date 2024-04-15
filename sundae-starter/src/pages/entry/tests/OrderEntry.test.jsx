fd49762a996dd07894f643a6b470f1d36918630f
Diego Ronan Kulian <dkulian@veepee.com>
2024-04-15 18:54:14 +0200

    testing disabled button

--- sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
+++ sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
@@ -19,3 +19,25 @@ test("handles error for scoops and toppings routes", async () => {
   const alerts = await screen.findAllByRole("alert");
   expect(alerts).toHaveLength(2);
 });
+
+test("disable order button if there are no scoops ordered", async () => {
+  const user = userEvent.setup();
+  render(<OrderEntry setOrderPhase={vi.fn()} />);
+
+  // order button should be disabled at first, even before options load
+  const orderButton = screen.getByRole("button", { name: /order sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  // expect button to be enabled after adding scoop
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(orderButton).toBeEnabled();
+
+  // expect button to be disabled again after removing scoop
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
+});
